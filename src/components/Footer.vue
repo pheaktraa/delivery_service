f1256cacb9ebd47b2f2770679b5e7d5689131464
Chet <zhongfyer@gmail.com>
2025-11-14 09:19:28 +0700

    make homepage

---
+++ src/components/Footer.vue
@@ -0,0 +1,72 @@
+<template>
+  <footer class="bg-black mt-[3rem]">
+    <div class="max-w-[80rem] m-auto h-[20rem]">
+      <div class="h-[15rem] flex justify-between py-[2rem]">
+        <div class="h-full flex items-center">
+          <img src="../assets/img/logo2.png" alt="" class="h-[7rem]" />
+        </div>
+        <div>
+          <p
+            class="text-white text-[length:var(--text-description)] font-semibold"
+          >
+            Navigation
+          </p>
+          <ul class="list-unstyled mt-[1rem] text-white">
+            <li
+              class="hover:text-[var(--red-800)] transition-colors duration-300"
+            >
+              Home
+            </li>
+            <li
+              class="hover:text-[var(--red-800)] transition-colors duration-300"
+            >
+              Create Delivery
+            </li>
+            <li
+              class="hover:text-[var(--red-800)] transition-colors duration-300"
+            >
+              My Deliveries
+            </li>
+            <li
+              class="hover:text-[var(--red-800)] transition-colors duration-300"
+            >
+              About Us
+            </li>
+            <li
+              class="hover:text-[var(--red-800)] transition-colors duration-300"
+            >
+              Contact
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p
+            class="text-white text-[length:var(--text-description)] font-semibold"
+          >
+            Address
+          </p>
+          <p class="text-white mt-[1rem]">
+            Street: #611, St.371 (Boeng Tompun) <br />S/K Stung Mean Chey, Khan
+            Mean Chey <br />
+            City: Khan Mean Chey <br />State/province/area: Phnom Penh
+          </p>
+        </div>
+        <div>
+          <p
+            class="text-white text-[length:var(--text-description)] font-semibold"
+          >
+            Social Media
+          </p>
+          <div class="flex mt-[1rem]">
+            <img src="../assets/img/facebookicon.png" alt="" class="h-[3rem]" />
+            <img src="../assets/img/instagramicon.png" alt="" class="h-[4rem] mt-[-.5rem]  ml-[-1rem]" />
+          </div>
+        </div>
+      </div>
+      <div class="w-full border-b-2 border-white"></div>
+      <p class="text-white my-7">
+        All rights reserved © 2025 deliveryManagement
+      </p>
+    </div>
+  </footer>
+</template>
