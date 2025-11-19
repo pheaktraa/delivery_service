4d5412200188fa708995d71e91d7e9a0ae153d6b
Chet <zhongfyer@gmail.com>
2025-11-20 01:51:38 +0700

    edit home page

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-[80rem] h-full m-auto mt-[5rem]">
+  <div class="max-w-[80rem] h-full p-[2rem] m-auto">
     <div class="flex">
       <div class="py-[12rem] w-full">
         <p
@@ -39,23 +39,23 @@
     </div>
     <p class="text-center text-[length:var(--text-title)] text-[var(--red-800)] font-semibold my-[3rem]">Why You Should Choose Our Service</p>
     <div class="grid md:grid-cols-2 gap-x-20 gap-y-10 relative">
-      <div class="w-[37rem] h-[15rem] bg-(--gray-200) rounded-[1rem] p-6 sd" data-aos="fade-down-right">
+      <div class="w-[33rem] h-[15rem] bg-(--gray-100) rounded-[1rem] p-6 sd" style="border: 2px solid var(--gray-200);" data-aos="fade-down-right">
         <p class="text-[length:var(--text-title)] font-semibold text-[var(--red-800)]">Fast <br>Delivery</p>
         <p class="mt-5 font-semibold ">Get your packages to their destination in record time with our optimized delivery routes and express options.</p>
       </div>
-      <div class="w-[37rem] h-[15rem] bg-(--gray-200) rounded-[1rem] p-6 sd" data-aos="fade-down-left">
+      <div class="w-[33rem] h-[15rem] bg-(--gray-100) rounded-[1rem] p-6 sd" style="border: 2px solid var(--gray-200);" data-aos="fade-down-left">
         <p class="text-[length:var(--text-title)] font-semibold text-[var(--red-800)]">Secure <br>Handling</p>
         <p class="mt-5 font-semibold ">Your items are protected at every step handled with care and delivered safely to your doorstep.</p>
       </div>
-      <div class="w-[37rem] h-[15rem] bg-(--gray-200) rounded-[1rem] p-6 sd" data-aos="fade-up-right">
+      <div class="w-[33rem] h-[15rem] bg-(--gray-100) rounded-[1rem] p-6 sd" style="border: 2px solid var(--gray-200);" data-aos="fade-up-right">
         <p class="text-[length:var(--text-title)] font-semibold text-[var(--red-800)]">Real-Time <br>Tracking</p>
         <p class="mt-5 font-semibold ">Stay updated anytime, anywhere. Track your shipment live and know exactly where it is.</p>
       </div>
-      <div class="w-[37rem] h-[15rem] bg-(--gray-200) rounded-[1rem] p-6 sd" data-aos="fade-up-left">
+      <div class="w-[33rem] h-[15rem] bg-(--gray-100) rounded-[1rem] p-6 sd" style="border: 2px solid var(--gray-200);" data-aos="fade-up-left">
         <p class="text-[length:var(--text-title)] font-semibold text-[var(--red-800)]">24/7 <br>Support</p>
         <p class="mt-5 font-semibold ">We’re always here for you. Our friendly support team is available day and night to help with any questions.</p>
       </div>
-      <div class=" h-[33rem] border-s-4 absolute left-1/2 top-0 border-[var(--gray-300)]"></div>
+      <div class=" h-[33rem] border-s-4 absolute left-[37rem] top-0 border-[var(--gray-300)]"></div>
     </div>
     <div class="mt-[10rem] grid md:grid-cols-2 gap-x-20">
       <div class="w-[37rem] h-[25rem]" data-aos="fade-up"  data-aos-offset="200">
