8c501f2564f5623919cd87e84524e0f170373c46
Pheaktra <130367888+pheaktraa@users.noreply.github.com>
2025-12-16 13:59:23 +0700

    fix ui managerUser and modal

--- src/components/ShareUserTable.vue
+++ src/components/ShareUserTable.vue
@@ -1,37 +1,37 @@
 <template>
-  <div class="bg-white rounded-xl shadow-sm border border-(--gray-100) mt-[2rem] overflow-hidden">
+  <div class="w-full h-full overflow-x-auto mt-[2rem] border border-(--gray-300) rounded-lg overflow-hidden">
 
     <!-- TABLE START -->
-    <table class="w-full text-left">
+    <table class="w-full border-collapse text-fixed">
 
       <!-- Table Header -->
-      <thead class="bg-(--gray-700) border-b border-(--gray-200)">
-        <tr>
+      <thead class="bg-(--gray-100) border-b border-(--gray-300)">
+        <tr class="bg-(--gray-500)">
           <!-- Fixed small width for ID -->
-          <th class="p-4 text-xs font-bold text-white uppercase w-[100px]">User ID</th>
+          <th class="p-4 text-left text-(--gray-100) font-bold">User ID</th>
           <!-- Allow Name to GROW (No width set) -->
-          <th class="p-4 text-xs font-bold text-white uppercase ">User Info</th>
+          <th class="p-4 text-left text-(--gray-100) font-bold">User Info</th>
           <!-- Fixed width for Status & Phone -->
-          <th class="p-4 text-xs font-bold text-white uppercase w-[180px]">Status</th>
-          <th class="p-4 text-xs font-bold text-white uppercase w-[180px]">Phone</th>
+          <th class="p-4 text-left text-(--gray-100) font-bold">Status</th>
+          <th class="p-4 text-left text-(--gray-100) font-bold">Phone</th>
           <!-- w-1 forces it to shrink -->
-          <th class="p-4 text-xs font-bold text-white w-1 whitespace-nowrap uppercase text-left">Action</th>
+          <th class="p-4 text-left text-(--gray-100) font-bold">Action</th>
         </tr>
       </thead>
 
       <!-- Table Body -->
-      <tbody class="divide-y divide-(--gray-200)">
-        <tr v-for="user in users" :key="user.id" class="hover:bg-(--gray-200) transition duration-300">
+      <tbody class="divide-y divide-(--gray-200) bg-white">
+        <tr v-for="user in users" :key="user.id" class="hover:bg-(--gray-100) transition duration-300">
 
           <!-- Column 1: ID -->
-          <td class="p-4 text-xs font-bold text-(--gray-800)">
+          <td class="p-4 text-(--gray-800)">
             {{ user.id }}
           </td>
 
           <!-- Column 2: Name & Role -->
-          <td class="p-4">
-            <div class="font-bold text-(--gray-800)">{{ user.name }}</div>
-            <div class="text-xs text-(--gray-800) capitalize">{{ user.role }}</div>
+          <td class="p-4 text-(--gray-800) font-bold">
+            {{ user.name }}
+            <!-- <div class="text-xs text-(--gray-800) capitalize">{{ user.role }}</div> -->
           </td>
 
           <!-- Column 3: Status Badge -->
@@ -46,12 +46,12 @@
           </td>
 
           <!-- Column 4: Phone -->
-          <td class="p-4 text-sm font-bold text-(--gray-800)">
+          <td class="p-4 text-(--gray-800) font-bold">
             {{ user.phone }}
           </td>
 
           <!-- Column 5: Actions -->
-          <td class="p-4 text-right flex gap-2">
+          <td class="p-4 flex gap-2">
 
             <!-- VIEW BUTTON (Opens the Modal) -->
             <button @click="$emit('view', user)"
